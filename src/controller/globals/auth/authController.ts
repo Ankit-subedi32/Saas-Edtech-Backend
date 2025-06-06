65e34f611e1b70340c9c3e44f1485d88035b3952
Ankit-subedi32 <ankit.subedi12322@gmail.com>
2025-06-06 23:35:13 +0545

    validation

--- src/controller/globals/auth/authController.ts
+++ src/controller/globals/auth/authController.ts
@@ -49,6 +49,20 @@ import bcrypt from "bcrypt"
 // BOLA Attack
 
 
+/**
+ login flow :
+ email/username and password (basic)
+email and password(data accept )--> validaition -->
+//first check email exist or not (verify) --> yes --> check password --> milyo vanya token(jsonwebtoken) jeneraton garn a parxa -->
+--> no --> not registered
+
+
+ google login ,facebook ,github(auth)
+ email login (SSO)
+
+ */
+
+
 class AuthController{
    static async registerUser(req:Request,res:Response){
     if(req.body == undefined){
@@ -82,6 +96,44 @@ class AuthController{
      res.status(201).json({
          message : "User registered successfully"
      })
+   }
+
+   async loginUser(req:Request,res:Response){
+    const {email,password} = req.body
+    if(!email || !password) {
+        res.status(400).json({
+            message : "Please provide email,password"
+        })
+        return 
+    }
+
+    //check if email exit or not in our table 
+    const data = await User.findAll({
+        where : {
+                email
+        }
+    })
+    //seletc * from User where email ="ankit@gmail.com" AND age == 23
+    if(data.length == 0 ){
+        res.status(404).json({
+            message : "Not registered"
+        })
+
+    }
+    else{
+        //check password , ankit123 (hanyako password ) --> hash conversion --> fsdkjjshguy
+        //compare (plain passsword) user bata aako password ,hashed password register huda
+        const isPasswordMatch = bcrypt.compareSync(password,data[0].password)
+        if(isPasswordMatch){
+            //login vayo ,token generate garnu paryo
+            
+
+        }else {
+            res.status(403).json({
+                message : " Invalid Email or passsword "
+            })
+        }
+    }
    }
 }
 
