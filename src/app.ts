42a7a4f8de98e9e47b884164aa24e02e60ed3ba3
Ankit-subedi32 <ankit.subedi12322@gmail.com>
2025-06-12 21:21:00 +0545

    create-institute

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 const app = express()
 import authRoute from "./route/globals/auth/authRoute"
+import instituteRoute from "./route/insititude/instituteRoute"
 
 app.use(express.json())
 // alternative body-parser
 
 app.use("/api",authRoute)
+app.use("/api/institute",instituteRoute)
 export default app
